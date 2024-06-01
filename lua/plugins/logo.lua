a2141f7bc864859d22ffcccb036341d65cca027d
gabriwar <gabrielduarteg02@gmail.com>
2024-06-01 10:30:51 -0300

    themed

--- lua/plugins/logo.lua
+++ lua/plugins/logo.lua
@@ -14,7 +14,8 @@ return {
 
             ]]
 
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      local currentDate = os.date("%x") -- Get current date in locale's date format
+      logo = string.rep("\n", 8) .. logo .. "\n\n" .. currentDate .. "\n\n"
       opts.config.header = vim.split(logo, "\n")
       opts.theme = "doom"
     end,
