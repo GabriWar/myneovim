744005a5cfacd40d4d47541749bae9cd037a46d5
gabriwar <gabrielduarteg02@gmail.com>
2024-06-01 11:09:46 -0300

    a

--- lua/plugins/logo.lua
+++ lua/plugins/logo.lua
@@ -14,7 +14,7 @@ return {
 
             ]]
 
-      local currentDate = os.date("%x") -- Get current date in locale's date format
+      local currentDate = os.date("%d/%m/%Y")
       logo = string.rep("\n", 8) .. logo .. "\n\n" .. currentDate .. "\n\n"
       opts.config.header = vim.split(logo, "\n")
       opts.theme = "doom"
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,3 +1,11 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim",
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("tokyonight").setup({
+      style = "night",
+    })
+    vim.cmd("colorscheme tokyonight")
+  end,
 }
