8f5d0bb964650977e428795c98459569f35cc052
gabriwar <gabrielduarteg02@gmail.com>
2024-06-01 20:25:27 -0300

    FINALLY ADDED CODE OUTLINE

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,12 @@
+return {
+  "hedyhli/outline.nvim",
+  config = function()
+    -- Example mapping to toggle outline
+    vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+
+    require("outline").setup({
+      outline_window = { focus_on_open = false },
+      preview_window = { auto_preview = true },
+    })
+  end,
+}
