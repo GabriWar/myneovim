695a7e1dbbfb952f7b54199a9aa5bf80ed339c6c
Gabriel Duarte Guerra <gabrielduarteg02@gmail.com>
2024-07-03 16:16:31 -0300

    fixed neotree ugly ass popups

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,19 +1,20 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    popup_border_style = "rounded",
     filesystem = {
       filtered_items = {
-	 visible = true,
-	 show_hidden_count = true,
-	 hide_dotfiles = false,
-	 hide_gitignored = true,
-	 hide_by_name = {
-	   -- '.git',
-	   -- '.DS_Store',
-	   -- 'thumbs.db',
-	 },
-	never_show = {},
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
       },
-    }
-  }
+    },
+  },
 }
