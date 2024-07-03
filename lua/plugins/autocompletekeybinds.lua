f85b874e807af0d555194eaf204fd301473e33fc
Gabriel Duarte Guerra <gabrielduarteg02@gmail.com>
2024-07-02 22:34:16 -0300

    trying to fit in this keybind

--- lua/plugins/autocompletekeybinds.lua
+++ lua/plugins/autocompletekeybinds.lua
@@ -7,7 +7,7 @@ return {
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-CR>"] = LazyVim.cmp.confirm({ select = true }),
+      ["<C-l>"] = LazyVim.cmp.confirm({ select = true }),
       ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
       ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
     }
