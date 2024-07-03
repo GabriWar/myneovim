bdd02a0807dfd31bee30095d492ae0783d1af326
Gabriel Duarte Guerra <gabrielduarteg02@gmail.com>
2024-07-02 21:10:58 -0300

    major rework

--- lua/plugins/compsourceemoji.lua
+++ lua/plugins/compsourceemoji.lua
@@ -4,5 +4,6 @@ return {
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     table.insert(opts.sources, { name = "emoji" })
+    table.insert(opts.sources, { name = "copilot" })
   end,
 }
