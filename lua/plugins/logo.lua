53e36103d2da95febebfc523330cf04c76e0de0d
Gabriel Duarte Guerra <gabrielduarteg02@gmail.com>
2024-12-30 19:09:36 -0300

    lazyvim got borking changes, fixed

--- lua/plugins/logo.lua
+++ lua/plugins/logo.lua
@@ -1,9 +1,9 @@
 return {
-  {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    opts = function(_, opts)
-      local logo = [[
+  "snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = [[
  ▄▀▀▀▀▄    ▄▀▀█▄   ▄▀▀█▄▄   ▄▀▀▄▀▀▀▄  ▄▀▀█▀▄   ▄▀▀▄    ▄▀▀▄  ▄▀▀█▄   ▄▀▀▄▀▀▀▄ 
 █         ▐ ▄▀ ▀▄ ▐ ▄▀   █ █   █   █ █   █  █ █   █    ▐  █ ▐ ▄▀ ▀▄ █   █   █ 
 █    ▀▄▄    █▄▄▄█   █▄▄▄▀  ▐  █▀▀█▀  ▐   █  ▐ ▐  █        █   █▄▄▄█ ▐  █▀▀█▀  
@@ -12,11 +12,8 @@ return {
 ▐         ▐   ▐   █    ▐   ▐     ▐   █       █         ▀    ▐   ▐   ▐     ▐   
                   ▐                  ▐       ▐                                
 
-            ]]
-
-      local currentDate = os.date("%d/%m/%Y")
-      logo = string.rep("\n", 8) .. logo .. "\n\n" .. currentDate .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
-    end,
+            ]],
+      },
+    },
   },
 }
